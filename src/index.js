a2faef6e37cf71a92ba639f502c0c60bf17e887f
Liza_Burak <b.a.z.i.liza007@gmail.com>
2021-08-15 22:58:15 +0300

    feat: implement functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let min =0;
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let max =0;
+  for (let i = 0; i < array.length; i++)
+  {
+      if (array[i] > max) {
+        max = array[i];
+      }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+
+  let sum = 0;
+  for (let i =0; i < array.length; i++)
+  {
+      sum+=array[i];
+  }
+
+  sum /= array.length;
+  return sum;
 }
